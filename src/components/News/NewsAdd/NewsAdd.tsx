3d904d68c55f0001fca94c79da5b1c4212fbab26
Sergey Sidorov <ssa-192@mail.ru>
2022-05-19 22:58:11 +0300

    Update NewsAdd.tsx

--- src/components/News/NewsAdd/NewsAdd.tsx
+++ src/components/News/NewsAdd/NewsAdd.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from "react";
+
 const NewsAdd = () => {
 
     const dateNow = () => {
@@ -5,12 +7,29 @@ const NewsAdd = () => {
         return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
     }
 
+    const [addNewsField, setAddNewsField] = useState("");
+
+    const handleSubmit = (event: FormEvent) => {
+      console.log(addNewsField);
+      setAddNewsField("");
+      event.preventDefault();
+    };
+  
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setAddNewsField(event.target.value);
+    };
+
     return (
         <>
-            <form>
-                <input type="text" placeholder="Добавить статью" />
-                <button type="submit">Добавить новость</button>
-            </form>
+<form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Добавить статью"
+          value={addNewsField}
+          onChange={handleChange}
+        />
+        <button type="submit">Добавить новость</button>
+      </form>
         </>
     );
 };

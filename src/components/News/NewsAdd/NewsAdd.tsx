29003e429784e42ab17f796b231b164947316a43
Sergey Sidorov <ssa-192@mail.ru>
2022-05-22 16:02:22 +0300

    Update addNews

--- src/components/News/NewsAdd/NewsAdd.tsx
+++ src/components/News/NewsAdd/NewsAdd.tsx
@@ -1,27 +1,35 @@
 import { FormEvent, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
+import { addNews } from "../../../redux/newsDataReducer/newsDataReducer";
 
 const NewsAdd = () => {
+  const { newsList } = useAppSelector((state) => state.newsList);
+  const dispatch = useAppDispatch();
 
-    const dateNow = () => {
-        const now = new Date();
-        return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+  const dateNow = () => {
+    const now = new Date();
+    return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+  }
+
+  const [addNewsField, setAddNewsField] = useState("");
+
+  const handleSubmit = (event: FormEvent): void => {
+    const news = {
+      date: dateNow(),
+      name: addNewsField,
     }
+    dispatch(addNews(news))
+    setAddNewsField("");
+    event.preventDefault();
+  };
 
-    const [addNewsField, setAddNewsField] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAddNewsField(event.target.value);
+  };
 
-    const handleSubmit = (event: FormEvent) => {
-      console.log(addNewsField);
-      setAddNewsField("");
-      event.preventDefault();
-    };
-  
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setAddNewsField(event.target.value);
-    };
-
-    return (
-        <>
-<form onSubmit={handleSubmit}>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Добавить статью"
@@ -30,8 +38,8 @@ const NewsAdd = () => {
         />
         <button type="submit">Добавить новость</button>
       </form>
-        </>
-    );
+    </>
+  );
 };
 
 export default NewsAdd;

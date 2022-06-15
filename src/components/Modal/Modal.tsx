b6cc8abcd48b3b645890150f164c9f692168c7dc
Sergey Sidorov <ssa-192@mail.ru>
2022-06-15 08:47:23 +0300

    Add Modal component animations

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,22 @@
 import { IModalComponent } from "../../types/types";
 
 const Modal = ({active, setActive}: IModalComponent) => {
-  const modalStyles = "bg-black-op w-screen h-screen fixed z-10";
+  const modalStyles = "bg-black-op w-screen h-0 fixed z-10 opacity-0";
+  const activeModalStyles = "bg-black-op w-screen h-screen fixed z-10 opacity-1 transition-opacity duration-300";
+
+  const modalContentStyles = "bg-white w-80 z-20 top-0 py-9 px-5 absolute left-2/4 -translate-x-2/4";
+  const activeModalContentStyles = "bg-white w-80 z-20 py-9 px-5 absolute left-2/4 top-1/4 -translate-x-2/4 transition-top duration-300";
 
   return (
-    <div onClick={() => setActive(false)} className={active ? modalStyles : modalStyles + ' hidden'}>
-      <div onClick={event => event.stopPropagation()} className="bg-white w-80 z-20 py-9 px-5 absolute left-2/4 top-1/4 -translate-x-2/4">
+    <div onClick={() => setActive(false)}
+    className={active ? activeModalStyles : modalStyles}
+
+    >
+      <div
+      onClick={event => event.stopPropagation()}
+      className={active ? activeModalContentStyles : modalContentStyles}
+
+      >
         <h2 className="font-bold text-lg mb-2 ml-2.5">Логин</h2>
         <input className="border border-grey-100 focus:outline-none w-full px-2.5 py-1.5 mb-5" type="text" />
         <h2 className="font-bold text-lg mb-2 ml-2.5">Пароль</h2>

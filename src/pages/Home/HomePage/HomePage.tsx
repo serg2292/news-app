7ab52b6ff17fb2edb641efe5a8a106b06f82b333
Sergey Sidorov <ssa-192@mail.ru>
2022-06-16 13:45:05 +0300

    Fix Home Page styles

--- src/pages/Home/HomePage/HomePage.tsx
+++ src/pages/Home/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <h1 className="text-6xl mt-24 mx-auto container text-center">Привет, Гость!</h1>;
+  return <h1 className="text-5xl mt-24 mx-auto container text-center">Привет, Гость!</h1>;
 };
 
 export default HomePage;

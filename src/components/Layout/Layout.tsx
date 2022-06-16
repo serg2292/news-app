31deea87d547ad103a0ece2d022312376fdf2fff
Sergey Sidorov <ssa-192@mail.ru>
2022-06-16 13:43:58 +0300

    Add Home Component styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,11 +7,11 @@ const LayoutPage = () => {
   const [modalActive, setModalActive] = useState(false)
 
   return (
-    <>
+    <div className="font-content">
       <Modal active={modalActive} setActive={setModalActive} />
       <Header active={modalActive} setActive={setModalActive} />
       <Outlet />
-    </>
+    </div>
   );
 };
 

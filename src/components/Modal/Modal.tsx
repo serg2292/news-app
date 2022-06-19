5e8739b32e008f57929c2fb8b3afc4fec3a96568
Sergey Sidorov <ssa-192@mail.ru>
2022-06-19 12:03:12 +0300

    Fix Modal styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,6 +7,7 @@ const Modal = ({active, setActive}: IModalComponent) => {
   const modalContentStyles = "bg-white w-80 z-20 top-0 py-9 px-5 absolute left-2/4 -translate-x-2/4";
   const activeModalContentStyles = "bg-white w-80 z-20 py-9 px-5 absolute left-2/4 top-1/4 -translate-x-2/4 transition-top duration-300";
 
+
   return (
     <div onClick={() => setActive(false)}
     className={active ? activeModalStyles : modalStyles}
@@ -16,10 +17,10 @@ const Modal = ({active, setActive}: IModalComponent) => {
       onClick={event => event.stopPropagation()}
       className={active ? activeModalContentStyles : modalContentStyles}
 
-      >
-        <h2 className="font-bold text-lg mb-2 ml-2.5">Логин</h2>
-        <input className="border border-grey-100 focus:outline-none w-full px-2.5 py-1.5 mb-5" type="text" />
-        <h2 className="font-bold text-lg mb-2 ml-2.5">Пароль</h2>
+      ><form onSubmit={(e) => e.preventDefault()}>
+        <label className="font-bold text-lg ml-2.5 inline-block mb-2">Логин</label>
+        <input className="border border-grey-100 focus:outline-none w-full px-2.5 py-1.5 mb-6" type="text" />
+        <label className="font-bold text-lg ml-2.5 inline-block mb-2">Пароль</label>
         <input className="border border-grey-100 focus:outline-none w-full px-2.5 py-1.5 mb-14" type="password" />
         <button
           className="font-semibold 
@@ -33,6 +34,7 @@ const Modal = ({active, setActive}: IModalComponent) => {
         transition 
         duration-100
         ">Вход</button>
+        </form>
       </div>
     </div>
   );

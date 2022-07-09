56400315082ae15c2d6b124c5ea9a9205b5ca09e
Sergey Sidorov <ssa-192@mail.ru>
2022-07-09 15:56:01 +0300

    change cmth

--- src/pages/Home/HomePage/HomePage.tsx
+++ src/pages/Home/HomePage/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   const authLogin = useAppSelector((state) => state.usersAuth.authLogin);
   return (
     <h1 className={HomepageHeaderStyle}>
-      Привет, {authLogin ? capitalize(authLogin) : "Гость"}!
+      Привет привет, {authLogin ? capitalize(authLogin) : "Гость"}!
     </h1>
   );
 };
